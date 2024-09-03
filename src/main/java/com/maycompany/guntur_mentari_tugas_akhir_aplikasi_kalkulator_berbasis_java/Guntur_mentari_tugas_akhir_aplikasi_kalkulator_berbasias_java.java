ad56ef9f032814d9b56294fcb8ae1ce474def534
ankIkan <ikanw50@gmail.com>
2024-09-03 22:22:27 +0800

    git config --global ankIkan
    iya

---
+++ src/main/java/com/maycompany/guntur_mentari_tugas_akhir_aplikasi_kalkulator_berbasis_java/Guntur_mentari_tugas_akhir_aplikasi_kalkulator_berbasias_java.java
@@ -0,0 +1,8 @@
+
+package com.maycompany.guntur_mentari_tugas_akhir_aplikasi_kalkulator_berbasis_java;
+
+public class Guntur_mentari_tugas_akhir_aplikasi_kalkulator_berbasias_java {
+    public static void main(String[] args) {
+       new tampilan_login().setVisible(true);
+    }
+}
